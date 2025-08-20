0859af5e521ef1ce3f19b2ff874b7598049adda7
Igor1 <chudilof001@gmail.com>
2025-08-20 11:46:56 +0300

    додав виправлення на 111 років

--- script/HW4.js
+++ script/HW4.js
@@ -22,7 +22,7 @@ if (age != +age){
     console.log("ІДИ ЗВІДСИ, БРЕХЛО!");
 }else if(+age >= 150){
     console.log("Вітаю тебе, довгожитель!")
-}else if(+age%10 == 1 && +age!=11){
+}else if(+age%100 == 1 && +age!=11){
     console.log("Тобі "+age+" рік!");
 }else if(+age%10>=2 && +age%10<=4 && !([12, 13, 14].includes(+age % 100))){
     console.log("Тобі "+age+" роки!")
